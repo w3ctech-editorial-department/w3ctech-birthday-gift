0667f87a0e6e66746da760ebb93b1c77763db148
liuliangsir <2269305724@qq.com>
2018-01-31 17:30:09 +0800

    Fix: :bug:修复一些bug

--- www/static/src/entry/home/saga.js
+++ www/static/src/entry/home/saga.js
@@ -3,7 +3,8 @@
  * @author liuliang<liuliang@w3ctech.com>
  */
 
-import {all, call, fork, put, select, take, takeEvery, takeLatest} from 'redux-saga/effects';
+import {all, call, fork, put, select, take} from 'redux-saga/effects';
+import {takeEvery, takeLatest} from 'redux-saga';
 
 
 export default function* rootSaga() {
